73a729afaaaf1c866dc808e9e2832b6a5ab6555e
saravutn001-ops <saravut.n001@gmail.com>
2026-08-22 10:10:49 +0700

    Update API URL in configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NF3_REPORT_CONFIG = {
-  apiUrl: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzbP4u5SZP9RLg11Bm8bNF8dRNUER9-0-HlKZ6bVR3RsdjdjYsowD0YoHQHDy7wVGA5/exec",
   appName: "NF3 Production Report V1"
 };
